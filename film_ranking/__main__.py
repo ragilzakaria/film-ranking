dccf23f40ca191180656f6719b824f9f41bbb778
Maulana Ragil Zakariah <m.zakariah@student.uw.edu.pl>
2024-07-09 00:50:00 +0200

    Framework for sqlite

--- film_ranking/__main__.py
+++ film_ranking/__main__.py
@@ -1,5 +1,5 @@
 from lib.load_data import load_movies_akas
 
 if __name__ == "__main__":
-    load_movies_akas("./data/akas.tsv")
+    load_movies_akas("./data/title.akas.tsv")
     print("Hello world!")
--- film_ranking/lib/load_data.py
+++ film_ranking/lib/load_data.py
@@ -1,14 +1,23 @@
 import csv
 import sqlite3
+import sys
+import pandas as pd
 
 DATABASE_NAME = "movies.db"
+csv.field_size_limit(sys.maxsize)
+
+
+def lazy_pandas_csv_reader(file_path, chunksize=1000):
+    for chunk in pd.read_csv(file_path, chunksize=chunksize, delimiter='\t'):
+        for row in chunk.itertuples(index=False, name=None):
+            yield row
 
 
 def create_table_movies_akas(cursor):
     # Create the table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS titles (
-            titleId TEXT,
+            titleId TEXT PRIMARY KEY,
             ordering INTEGER,
             title TEXT,
             region TEXT,
@@ -20,25 +29,35 @@ def create_table_movies_akas(cursor):
     ''')
 
 
-def ingest_movies_akas(cursor, file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter='\t')
-        data = [(row['titleId'], row['ordering'], row['title'], row['region'], row['language'], row['types'],
-                 row['attributes'], row['isOriginalTitle']) for row in reader]
-
-        cursor.executemany('''
+def ingest_movies_akas(conn, cursor, file_path):
+    for row in lazy_pandas_csv_reader(file_path):
+        upsert_query = '''
             INSERT INTO titles (titleId, ordering, title, region, language, types, attributes, isOriginalTitle)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        ''', data)
+            ON CONFLICT(titleId) DO UPDATE SET
+                ordering=excluded.ordering,
+                title=excluded.title,
+                region=excluded.region,
+                language=excluded.language,
+                types=excluded.types,
+                attributes=excluded.attributes,
+                isOriginalTitle=excluded.isOriginalTitle
+        '''
+        cursor.execute(upsert_query, row)
+        # Optionally, commit periodically to avoid holding too many uncommitted rows in memory
+        if cursor.lastrowid % 1000 == 0:
+            conn.commit()
+            # never call conn.close() from here!
 
 
 def load_movies_akas(file_path: str):
     conn = sqlite3.connect(f"./processed_data/{DATABASE_NAME}")
     cursor = conn.cursor()
     create_table_movies_akas(cursor)
-    ingest_movies_akas(cursor, file_path)
-    # Commit the transaction
     conn.commit()
+    ingest_movies_akas(conn, cursor, file_path)
+    # Commit the transaction
+
     # Close the connection
     conn.close()
 
